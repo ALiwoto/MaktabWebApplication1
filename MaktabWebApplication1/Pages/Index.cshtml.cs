a809d1de1bdf967bde9cf36424f9edc6ffeb0768
Aliwoto <woto@kaizoku.cyou>
2023-02-07 18:44:13 +0330

    Add project files.

---
+++ MaktabWebApplication1/Pages/Index.cshtml.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MaktabWebApplication1.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        public string MyName { get; set; }
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+
+        static HttpClient myAppHTTPClient = new HttpClient();
+
+        public async void OnPostProcessRequestAsync()
+        {
+            string firstName, lastName, email;
+            string host = "https://thirdparty.app.com:443/";
+            string pathname = "path/to/api/endpoint/?operation=create";
+
+            firstName = "Test";
+            lastName = "User";
+            email = "TestUser@email.com";
+
+            string path = pathname + "&first_name=" + firstName + "&last_name=" + lastName + "&email=" + email;
+            string requestUrl = host + path;
+
+            HttpRequestMessage httpRequestMessage = new HttpRequestMessage();
+
+            try
+            {
+                HttpResponseMessage responseMessage = await myAppHTTPClient.PostAsync(requestUrl, httpRequestMessage.Content);
+                HttpContent content = responseMessage.Content;
+                string message = await content.ReadAsStringAsync();
+                Console.WriteLine("The output from thirdparty is: {0}", message);
+                RedirectToPage();
+            }
+            catch (HttpRequestException exception)
+            {
+                Console.WriteLine("An HTTP request exception occurred. {0}", exception.Message);
+            }
+        }
+
+        public string GetName()
+        {
+            return "Ali Abedini";
+        }
+    }
+}
---
+++ MaktabWebApplication1/Services/GetDataController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MaktabWebApplication1.Services
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GetDataController : ControllerBase
+    {
+        [HttpGet]
+        public string GetData() => "Ali Abedini";
+    }
+}
